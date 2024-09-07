ff2d00633143ead115483e595d875950e910d089
Johann Neumann <johannneumann72@gmail.com>
2024-09-08 02:52:30 +0500

    event button click

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -9,5 +9,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-course';
+  onButtonClick() {
+    console.log('Button is clicked');
+  }
 }
