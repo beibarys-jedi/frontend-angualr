f204fefc9d288557bac781cc5c400b584249624c
Johann Neumann <johannneumann72@gmail.com>
2024-09-08 22:50:55 +0500

    update the bugfix of event and conditional logic

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -9,20 +9,29 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  length = 0;
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
 
   password = '';
 
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
   onChangeUseLetters() {
-    this.includeLetters != this.includeLetters;
+    this.includeLetters = !this.includeLetters;
   }
   onChangeUseNumbers() {
-    this.includeNumbers != this.includeNumbers;
+    this.includeNumbers = !this.includeNumbers;
   }
   onChangeUseSymbols() {
-    this.includeSymbols != this.includeSymbols;
+    this.includeSymbols = !this.includeSymbols;
   }
 
   onButtonClick() {
